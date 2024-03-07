5d14e6e828dcbac54505b8e9efd707e66516011e
0xPrakharG <prakhargoyal.3014@gmail.com>
2024-03-07 22:42:17 +0530

    feat: query string search

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,5 +1,18 @@
-const Search = () => {
-  return <div>Search</div>;
+import { db } from "@/lib/db";
+import Categories from "./_components/categories";
+
+const SearchPage = async () => {
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+  
+  return (
+    <div className="p-6">
+      <Categories items={categories} />
+    </div>
+  );
 };
 
-export default Search;
+export default SearchPage;
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ import { Sidebar } from "./_components/sidebar";
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
-      <div className="h-[80px] md:pl-6 fixed inset-y-0 w-full z-50">
+      <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
         <Navbar />
       </div>
       <div className="hidden w-56 md:flex h-full  flex-col fixed inset-y-0 z-50">
